18236eecde58a73ea0b4c336faa18a2ef3046707
naresh1318 <rnaresh.n@gmail.com>
2019-03-09 19:12:37 -0500

    initial commit

---
+++ batch_scripts/start_p300_acquisition.cmd
@@ -0,0 +1,1 @@
+C:\"Program Files"\openvibe-2.2.0-64bit\openvibe-designer.cmd --no-gui --play="C:\Users\Naresh\AppData\Roaming\openvibe-2.2.0\scenarios\bci-examples\p300-speller-xDAWN\p300-xdawn-1-acquisition.xml"
---
+++ batch_scripts/start_signal_monitoring.cmd
@@ -0,0 +1,1 @@
+C:\"Program Files"\openvibe-2.2.0-64bit\openvibe-designer.cmd --play="C:\Program Files\openvibe-2.2.0-64bit\share\openvibe\scenarios\bci-examples\p300-speller-xDAWN/p300-xdawn-0-signal-monitoring.xml" --no-gui
