aad4bc1f7a7c2728a8a9add0e21434b89ce37d95
naresh1318 <rnaresh.n@gmail.com>
2019-03-25 21:26:43 -0400

    word reset on each online click

--- js/p300_speller.js
+++ js/p300_speller.js
@@ -286,6 +286,8 @@ let app = new Vue({
                 return;
             }
 
+            app.p300_word_constructed = "";
+
             const speller_matrix = [["a", "b", "c", "d", "e", "f"],
                                   ["g", "h", "i", "j", "k", "l"],
                                   ["m", "n", "o", "p", "q", "r"],
