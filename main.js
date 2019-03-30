57a68a670abb3aed65473ea22bb90960cea699c2
naresh1318 <rnaresh.n@gmail.com>
2019-03-29 22:12:58 -0400

    images added and version 1.2.0

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let {app, BrowserWindow} = require('electron');
-// require('electron-reload')(__dirname, {ignored: "/openvibe_scenarios|[\/\\]\./"});
+// require('electron-reload')(__dirname);
 
 let win = null;
 
